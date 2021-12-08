2d824cd0149ae27810cb7c3af2795b50dbfa2f6a
tom <dont@mail.me>
2021-12-08 14:03:20 +0100

    add relevant data to 'party' and 'track' fixtures

--- src/DataFixtures/PartyFixtures.php
+++ src/DataFixtures/PartyFixtures.php
@@ -5,16 +5,21 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Party;
+use Faker\Factory;
 
 class PartyFixtures extends Fixture
 {
+    protected $faker;
+
     public function load(ObjectManager $manager): void
     {
-        for( $i = 0 ; $i < 25 ; $i++) {
-            $party = new Party();
-            $party->setCurrentTrack($i);
+        $this->faker = Factory::create();
 
-            $date = new \DateTime();
+        for( $i = 0 ; $i < 10 ; $i++) {
+            $party = new Party();
+            $party->setCurrentTrack($this->faker->numberBetween(0, 5));
+
+            $date = $this->faker->dateTimeBetween('-10 days', '+50 days');
             $party->setDate($date);
             
             $end_date = clone $date;
--- src/DataFixtures/TrackFixtures.php
+++ src/DataFixtures/TrackFixtures.php
@@ -11,6 +11,28 @@ class TrackFixtures extends Fixture
 {
     protected $faker;
 
+    private static $paths = [
+        'https://ia800905.us.archive.org/19/items/FREE_background_music_dhalius/BackgroundMusica2.ogg',
+        'https://ia800905.us.archive.org/19/items/FREE_background_music_dhalius/Director.ogg',
+        'https://ia800905.us.archive.org/19/items/FREE_background_music_dhalius/FaceBangSonic.ogg',
+        'https://ia800905.us.archive.org/19/items/FREE_background_music_dhalius/FormulaFantasy.ogg',
+        'https://ia800905.us.archive.org/19/items/FREE_background_music_dhalius/Hangout.ogg'
+    ];
+
+    private static $thumbnails = [
+        'https://i.ytimg.com/vi/jiwuQ6UHMQg/mqdefault.jpg',
+        'https://i.ytimg.com/vi/BiqlZZddZEo/mqdefault.jpg',
+        'https://i.ytimg.com/vi/Ps638erKAn4/mqdefault.jpg',
+        'https://i.ytimg.com/vi/uT3SBzmDxGk/mqdefault.jpg'
+    ];
+
+    private static $states = [
+        'TO_LOAD',
+        'DOWLOADING',
+        'READY',
+        'ON_ERROR'
+    ];
+
     public function load(ObjectManager $manager): void
     {
         $this->faker = Factory::create();
@@ -26,9 +48,9 @@ class TrackFixtures extends Fixture
             $track->setTrackId($uuid);
 
             $track->setTitle($this->faker->name);
-            $track->setPath($this->faker->url);
-            $track->setThumbnailPath($this->faker->url);
-            $track->setState('fake_state');
+            $track->setPath($this->faker->randomElement(self::$paths));
+            $track->setThumbnailPath($this->faker->randomElement(self::$thumbnails));
+            $track->setState($this->faker->randomElement(self::$states));
 
             $manager->persist($track);
         }
