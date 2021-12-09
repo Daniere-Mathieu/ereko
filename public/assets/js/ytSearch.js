256a43febc4bac5217ab3fa5a73cfa18436c5b6a
Neirotciv <tanacetifolia@gmail.com>
2021-12-10 00:24:37 +0100

    code cleanup

--- public/assets/js/ytSearch.js
+++ public/assets/js/ytSearch.js
@@ -3,27 +3,22 @@ let result_list = document.getElementById('result_list');
 let list_exist = false;
 
 function createResultDiv(item) {
-    let result = document.createElement('div');
-    result.className = 'result';
-    console.log(result);
-
-    let result_img = document.createElement('div');
-    result_img.className = 'result_img';
-
     let img = document.createElement('img');
     img.src = item.snippet.thumbnails.medium.url;
-    result_img.appendChild(img);
-    result_img.alt = "Thumbnail of Youtube video" 
-    
-    let result_info = document.createElement('div');
-    result_info.className = 'result_info';
+    img.alt = "Thumbnail of Youtube video";
+
+    let result_img = document.createElement('div').appendChild(img);
+    result_img.className = 'result_img';
 
     let result_title = document.createElement('p');
     result_title.className = 'result_title';
     result_title.innerHTML = titleInBold(item.snippet.title);
 
-    result_info.appendChild(result_title);
+    let result_info = document.createElement('div').appendChild(result_title);
+    result_info.className = 'result_info';
 
+    let result = document.createElement('div');
+    result.className = 'result';
     result.appendChild(result_img);
     result.appendChild(result_info);
 
@@ -33,7 +28,6 @@ function createResultDiv(item) {
 function removeResultDiv() {
     let result_div = document.getElementsByClassName("result");
     for (let i = result_div.length-1; i >= 0 ; i--) {
-        console.log(i);
         result_list.removeChild(result_div[i]);
     }
 }
@@ -42,7 +36,7 @@ function titleInBold(title) {
     let found = title.match('[-]');
     if (found != null) {
         let splitTitle = title.split(' -');
-        return '<b>' + splitTitle.shift() + ' -</b>' + splitTitle.join(' -');
+        return '<b>' + splitTitle.shift() + '</b> -' + splitTitle.join(' -');
     }
     return title;
 }
@@ -59,7 +53,7 @@ async function requestToYoutube(research) {
     };
 
     Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
-    console.log(url);
+    
     await fetch(url, {headers: {'Accept': 'application/json'}})
         .then(function (response) {
             if (response.status == 200) {
@@ -70,7 +64,6 @@ async function requestToYoutube(research) {
         })
         .then(function (data) {
             for (const item of data.items) {
-                console.log(item);
                 createResultDiv(item);
             }
         })
