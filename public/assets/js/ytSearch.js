74e2b9f8535275be00fd0ab5eeeef4abd900dc13
Neirotciv <tanacetifolia@gmail.com>
2021-12-09 14:53:37 +0100

    create result div with js

--- public/assets/js/ytSearch.js
+++ public/assets/js/ytSearch.js
@@ -1,4 +1,38 @@
 let search_input = document.getElementById('search_input');
+let result_list = document.getElementById('result_list');
+
+function createResultDiv() {
+    let result = document.createElement('div');
+    result.className = 'result';
+    console.log(result);
+
+    let result_img = document.createElement('div');
+    result_img.className = 'result_img';
+
+    let img = document.createElement('img');
+    img.src = "https://i.ytimg.com/vi/BmSzWXl5Ofg/mqdefault.jpg";
+    result_img.appendChild(img);
+    // Ajouter img alt
+
+    let result_info = document.createElement('div');
+    result_info.className = 'result_info';
+
+    let result_title = document.createElement('p');
+    result_title.className = 'result_title';
+    result_title.innerHTML = "<b>Jackson 5</b>- Eye of the tiger 1 aaaaaaaaaaaa fdsgfdg gfdgfdggfdg gfdgfdg gfdgfdg gfdgfdg gfdgfdg ";
+
+    let result_time = document.createElement('p');
+    result_time.innerText = "03:45";
+
+    result_info.appendChild(result_title);
+    result_info.appendChild(result_time);
+
+    
+    result.appendChild(result_img);
+    result.appendChild(result_info);
+
+    result_list.appendChild(result);
+}
 
 search_input.addEventListener('keyup', (e) => {
     if (e.key === 'Enter') {
@@ -8,3 +42,8 @@ search_input.addEventListener('keyup', (e) => {
     }
 })
 
+createResultDiv()
+createResultDiv()
+createResultDiv()
+createResultDiv()
+createResultDiv()
