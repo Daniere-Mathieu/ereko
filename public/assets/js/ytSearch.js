7c33c01d4765d83123148f73244103cda71045c6
Neirotciv <tanacetifolia@gmail.com>
2021-12-09 15:27:43 +0100

    create request to youtube

--- public/assets/js/ytSearch.js
+++ public/assets/js/ytSearch.js
@@ -1,12 +1,12 @@
 let search_input = document.getElementById('search_input');
 let result_list = document.getElementById('result_list');
 
-function createResultDiv() {
+function createResultDiv(item) {
     let result = document.createElement('div');
     result.className = 'result';
     console.log(result);
 
-    let result_img = document.createElement('div');
+    let result_img = document.createElement('div')
     result_img.className = 'result_img';
 
     let img = document.createElement('img');
@@ -32,6 +32,39 @@ function createResultDiv() {
     result.appendChild(result_info);
 
     result_list.appendChild(result);
+}
+
+async function requestToYoutube(research) {
+    let url = new URL('https://www.googleapis.com/youtube/v3/search?');
+    let params = {
+        'part': 'snippet',
+        'q': research,
+        'maxResults': 5,
+        'type': 'video',
+        'videoDuration': 'medium', // Entre 4 et 20 mn
+        'key': API_KEY,
+    };
+
+    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+    console.log(url);
+    await fetch(url, {headers: {'Accept': 'application/json'}})
+        .then(function (response) {
+            if (response.status == 200) {
+                return response.json();
+            } else {
+                throw new TypeError('Request failed ! Status code : ' + response.status);
+            }
+        })
+        .then(function (data) {
+            for (const item of data.items) {
+                console.log(item);
+            }
+        })
+        .catch(function (e) {
+            console.log(e);
+            // A FAIRE : afficher une erreur dans le front
+        }
+    );
 }
 
 search_input.addEventListener('keyup', (e) => {
