09da4dab8db424535d65b43f0ac85e26f31490ec
Neirotciv <tanacetifolia@gmail.com>
2021-12-15 13:55:31 +0100

    update svg css & add error svg

--- public/assets/js/Track.js
+++ public/assets/js/Track.js
@@ -5,6 +5,7 @@ class Track {
   order;
   state_track;
   download_path;
+  
   constructor(party_id, track_id, state_for_party, order, state_track, download_path) {
     this.party_id = party_id;
     this.track_id = track_id;
@@ -23,11 +24,14 @@ class Track {
     track.appendChild(title);
     title.setAttribute("class","title_track")
     title.setAttribute("id",number)
+
     title.innerText = titleParam + " | " + this.state_track;
     
     console.log(this.state_track);
-    if (this.state_track === 'ON_ERROR') {
+    if (this.state_track === 'DOWNLOADING') {
       track.appendChild(this.displayDownloadSVG());
+    } else if (this.state_track === 'ON_ERROR') {
+      track.appendChild(this.displayErrorSVG());
     }
   }
 
@@ -38,8 +42,8 @@ class Track {
 
   displayDownloadSVG() {
     let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    svg.classList = "refresh_logo";
-    svg.setAttribute('viewBox', '0 0 24 24');
+    svg.setAttribute('viewBox', '0 0 24 24')
+    svg.classList = 'track_state_svg rotate';
 
     let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
     path.setAttribute('d', 'M0 0h24v24H0z');
@@ -56,14 +60,48 @@ class Track {
     svg.appendChild(path1);
     svg.appendChild(path2);
 
-    return svg;
+    let div = document.createElement('div');
+    div.classList = 'container_logo';
+    div.appendChild(svg);
+    
+    return div;
   }
 
+  displayErrorSVG() {
+    let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute('viewBox', '0 0 24 24')
+    svg.classList = 'track_state_svg';
 
+    let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute('stroke', 'none');
+    path.setAttribute('d', 'M0 0h24v24H0z');
+    path.setAttribute('fill', 'none');
+
+    let circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    circle.setAttribute('cx', '12');
+    circle.setAttribute('cy', '12');
+    circle.setAttribute('r', '9');
+    
+    let line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    line.setAttribute('x1', '9');
+    line.setAttribute('y1', '15');
+    line.setAttribute('x2', '15');
+    line.setAttribute('y2', '9');
+
+    svg.appendChild(path);
+    svg.appendChild(circle);
+    svg.appendChild(line);
+
+    let div = document.createElement('div');
+    div.classList = 'container_logo';
+    div.appendChild(svg);
+    
+    return div;
+  }
 }
 
-{/* <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-forbid-2" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
-  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-  <circle cx="12" cy="12" r="9" />
-  <line x1="9" y1="15" x2="15" y2="9" />
-</svg> */}
+
+//   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+//   <circle cx="12" cy="12" r="9" />
+//   <line x1="9" y1="15" x2="15" y2="9" />
+// </svg> */}
