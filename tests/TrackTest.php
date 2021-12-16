74661e18c8c5ab09b32d278f5e2216c02f0aee1a
tom <dont@mail.me>
2021-12-16 16:52:40 +0100

    test download retry on error

--- tests/TrackTest.php
+++ tests/TrackTest.php
@@ -10,6 +10,7 @@ class TrackTest extends TestCase
 {
     private $forbidden_state_error_message = 'Track state forbidden';
     private $wrong_uid_error_message = 'Wrong track uid.';
+    private $reflection_track;
 
     public function testSettersAndGetters(): void
     {
@@ -90,5 +91,98 @@ class TrackTest extends TestCase
             $this->expectExceptionMessage($this->wrong_uid_error_message);
             Track::verifyMatchUid($uid);
         }
+    }
+
+    public function testShouldBeDownloadedWithoutTimeConcern() {
+        $track = new Track();
+        $states_false = ['READY', 'TOO_LONG'];
+        $states_true = ['TO_DOWNLOAD', 'DOWNLOADING', 'ON_ERROR'];
+
+        foreach ($states_false as $state) {
+            $track->setState($state);
+            $this->assertFalse($track->ShouldBeDownloaded());
+        }
+
+        foreach ($states_true as $state) {
+            $track->setState($state);
+            $this->assertTrue($track->ShouldBeDownloaded());
+        }
+    }
+
+    public function testShouldBeDownloadedWithTimeConcernReturnTrue() {
+
+        $error_retries_values = [ 0, 1, 4, 5, 6, 19, 20, 21, 39, 40, 41, 99, 100, 101, 102];
+
+        // make private properties accessible.
+        // see https://www.php.net/manual/en/reflectionproperty.setvalue.php
+        // see also https://www.webtipblog.com/unit-testing-private-methods-and-properties-with-phpunit/
+        $this->reflection_track = new \ReflectionClass(Track::class);
+        $track_updated = $this->reflection_track->getProperty('updated');
+        $track_updated->setAccessible(true);
+        $track_retries = $this->reflection_track->getProperty('error_retries');
+        $track_retries->setAccessible(true);
+
+        foreach ($error_retries_values as $retries) {
+
+            $track = new Track();
+            $track->setState('ON_ERROR');
+            $track_retries->setValue($track, $retries);
+
+            switch(true) {
+                case ($retries <= 5):
+                    $delays = [
+                        "P0DT0H0M40S" => true, // -31 seconds
+                        "P0DT0H0M31S" => true, // -31 seconds
+                        "P0DT0H0M30S" => true, // 30 seconds
+                        "P0DT0H0M29S" => false, // 31
+                        "P0DT0H0M10S" => false,
+                    ];
+                    break;
+                case ($retries <= 20):
+                    $delays = [
+                        "P0DT0H0M59S" => false, // 59 sec
+                        "P0DT0H1M0S" => true, // 1 min
+                        "P0DT0H1M1S" => true // 1 min 1 sec
+                    ];
+                    break;
+                case ($retries <= 40):
+                    $delays = [
+                        "P0DT0H4M59S" => false, // 4:59 min
+                        "P0DT0H5M0S" => true, // 5 min
+                        "P0DT0H5M1S" => true // 5 min 1 sec
+                    ];
+                    break;
+                case ($retries <= 100):
+                    $delays = [
+                        "P0DT0H59M59S" => false, // 59 min 59
+                        "P0DT1H0M0S" => true, // 1 hour
+                        "P0DT1H0M1S" => true // 1 hour 1 sec
+                    ];
+                    break;
+                default:
+                    // when more than 100 retries, always abort
+                    $delays = [
+                        "P0DT0H0M0S" => false,
+                    ];
+                    break;
+            }
+
+            foreach($delays as $delay => $expected_answer) {
+                    $this->assertSame(
+                    $expected_answer,
+                    $this->setMockUpdatedAndCompare($track, $track_updated, $retries, $delay)
+                );
+            }
+        }
+    }
+
+    private function setMockUpdatedAndCompare(Track $track, $track_updated, $retries, String $delay) {
+
+        $now = new \Datetime();
+        $delay = new \DateInterval($delay);
+        $mock_updated = $now->sub($delay);
+        $track_updated->setValue($track, $mock_updated);
+
+        return $track->shouldBeDownloaded();
     }
 }
