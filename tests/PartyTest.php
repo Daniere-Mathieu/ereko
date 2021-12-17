679c0bb5500eed37905daeb32ea4518590b36724
tom <dont@mail.me>
2021-12-17 10:22:27 +0100

    Test Party : maximum number of tracks

--- tests/PartyTest.php
+++ tests/PartyTest.php
@@ -4,24 +4,32 @@ namespace App\Tests;
 
 use PHPUnit\Framework\TestCase;
 use App\Entity\Party;
+use App\Entity\TrackInParty;
 
 class PartyTest extends TestCase
 {
     public function testLimitNumberOfTracksUntilLimit(): void
     {
-        $party = new Party();
+        
         $test_numbers = [1, 10, 199, 200, 201, 250];
         $limit = 200;
 
         foreach ($test_numbers as $tracks_number) {
-            if ($tracks_number > $limit) {
-                $this->expectException( "InvalidArgumentException" );
-                $this->expectExceptionMessage($this->forbidden_state_error_message);
+            echo 'case: ' . $tracks_number . "\n";
+
+            $party = new Party();
+            for($i = 0 ; $i < $tracks_number ; $i++) {
+                $track_in_party = new TrackInParty();
+                $party->addTrackInParty($track_in_party);
             }
-            $this->addTracksToParty()
+            echo "track number: " . count($party->getTrackInParties()) . "\n";
+
+            if ($tracks_number >= $limit) {
+                $this->assertTrue($party->trackNumberLimitIsReached());
+            }
+            else {
+                $this->assertFalse($party->trackNumberLimitIsReached());
+            }
         }
-
-
-        $this->assertTrue(true);
     }
 }
