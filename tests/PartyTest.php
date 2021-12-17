4310c6107eb2c5462516ef79b1249843b7d45c39
tom <dont@mail.me>
2021-12-17 10:22:01 +0100

    DRAFT begin creating Party tests

--- tests/PartyTest.php
+++ tests/PartyTest.php
@@ -7,14 +7,18 @@ use App\Entity\Party;
 
 class PartyTest extends TestCase
 {
-    public function testLimitNumberOfTracks(): void
+    public function testLimitNumberOfTracksUntilLimit(): void
     {
         $party = new Party();
         $test_numbers = [1, 10, 199, 200, 201, 250];
         $limit = 200;
 
         foreach ($test_numbers as $tracks_number) {
-
+            if ($tracks_number > $limit) {
+                $this->expectException( "InvalidArgumentException" );
+                $this->expectExceptionMessage($this->forbidden_state_error_message);
+            }
+            $this->addTracksToParty()
         }
 
 
